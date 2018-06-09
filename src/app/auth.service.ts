201bc9bf76b4166e2c97970608fed62ea67fc691
ctung <chuck.tung@gmail.com>
2018-06-08 22:00:13 -0400

    working authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,8 +8,9 @@ import { WebsocketService } from './websocket.service';
 export const TOKEN_NAME = 'api_password';
 
 @Injectable()
-export class AuthService  {
+export class AuthService {
   private loggedIn = new BehaviorSubject<boolean>(false); // {1}
+  private api_password: string;
 
   get isLoggedIn() {
     return this.loggedIn.asObservable(); // {2}
@@ -18,29 +19,45 @@ export class AuthService  {
   constructor(
     private router: Router,
     private wsService: WebsocketService
-  ) { }
+  ) {
+    this.api_password = localStorage.getItem('api_password');
+    this.socketHandler();
+  }
+
+  socketHandler() {
+    this.wsService.getSocket()
+      .subscribe(data => {
+        const resp = JSON.parse(data);
+        // console.log(data);
+        switch (resp.type) {
+          case 'auth_ok':
+            this.loggedIn.next(true);
+            this.router.navigate(['/']);
+            break;
+          case 'auth_required':
+            if (this.api_password) {
+              this.wsService.sendMessage(JSON.stringify({ type: 'auth', api_password: this.api_password }));
+            } else {
+              this.loggedIn.next(false);
+              this.router.navigate(['/login']);
+            }
+            break;
+          case 'auth_invalid':
+            alert(resp.message);
+            localStorage.removeItem('api_password');
+            this.loggedIn.next(false);
+            this.router.navigate(['/login']);
+        }
+      });
+  }
 
   login(user: User) {
     if (user.password !== '') { // {3}
-      this.wsService.getSocket()
-      .subscribe(data => {
-        const resp = JSON.parse(data);
-        console.log(data);
-        if (resp.type === 'auth_ok') {
-          localStorage.setItem('api_password', user.password);
-          this.loggedIn.next(true);
-          this.router.navigate(['/']);
-        } else {
-          alert(resp.message);
-          localStorage.removeItem('api_password');
-          this.loggedIn.next(false);
-          this.router.navigate(['/login']);
-        }
-      });
+      this.api_password = user.password;
+      localStorage.setItem('api_password', user.password);
       this.wsService.sendMessage(JSON.stringify({ type: 'auth', api_password: user.password }));
     }
   }
-
 
   logout() {                            // {4}
     localStorage.removeItem('api_password');
