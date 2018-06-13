b958ac57a3f6a287a6e46750019aab60b7ba2108
ctung <chuck.tung@gmail.com>
2018-06-13 00:59:56 -0400

    add svg floorplan and snap.svg

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+declare var Snap: any;
 
 @Component({
   selector: 'app-map',
@@ -10,10 +12,12 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const s = Snap('#svgout');
+    const g = s.group();
+    const fplan = Snap.load('../../assets/cape_fplan2.svg', (f) => {
+        g.append(f);
+        s.select('#rect5204').click(() => this.myEvent.emit('kitchen') );
+    });
   }
 
-  click() {
-    // console.log('click');
-    this.myEvent.emit('Bathroom');
-  }
 }
