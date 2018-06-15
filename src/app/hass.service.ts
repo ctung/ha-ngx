09d6ec24e0b13e0a681c8ead465b2d09ac0bc7a0
ctung <chuck.tung@gmail.com>
2018-06-15 01:05:41 -0400

    refactor hassService

--- src/app/hass.service.ts
+++ src/app/hass.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { WebsocketService } from './websocket.service';
 import { BehaviorSubject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,41 +9,62 @@ import { filter, map } from 'rxjs/operators';
 export class HassService {
   // store callbacks that can be run on socket responses, if the id matches
   private id = 0;
+  private msgHandler: any = {};
 
   // store the hass config and state
   // uses method from https://coryrylan.com/blog/angular-observable-data-services
-  config: any;
   private _states: BehaviorSubject<any[]>;
+  private _services: BehaviorSubject<any[]>;
+  private _config: BehaviorSubject<any>;
   private dataStore: {
-    states: any[]
+    states: any[],
+    services: any[],
+    config: any
   };
 
   constructor(
     private wsService: WebsocketService
   ) {
-    this.dataStore = { states: [] };
+    this.dataStore = { states: [], services: [], config: {} };
     this._states = <BehaviorSubject<any[]>>new BehaviorSubject([]);
-    this.loadStates();
+    this._services = <BehaviorSubject<any[]>>new BehaviorSubject([]);
+    this._config = <BehaviorSubject<any>>new BehaviorSubject([]);
+    this.socketHandler();
+    this.load('states', this._states);
+    this.load('services', this._services);
+    this.load('config', this._config);
+    // this.call('light', 'turn_off');
   }
 
-  get states() {
-    return this._states.asObservable();
+  get states() { return this._states.asObservable(); }
+  get services() { return this._services.asObservable(); }
+  get config() { return this._config.asObservable(); }
+
+  private load(varName: string, bs: any) {
+    const myId = (+ new Date()) * 1000 + this.id++ % 1000;
+    const msg = { id: myId, type: 'get_' + varName };
+    this.msgHandler[myId] = (data) => {
+      console.log(data);
+      this.dataStore[varName] = data.result;
+      bs.next(Object.assign({}, this.dataStore)[varName]);
+    };
+    this.wsService.sendMessage(JSON.stringify(msg));
   }
 
-  loadStates() {
-    const myId = this.getId();
+  call(domain: string, service: string) {
+    const myId = (+ new Date()) * 1000 + this.id++ % 1000;
+    const msg = {id: myId, type: 'call_service', domain: domain, service: service};
+    this.wsService.sendMessage(JSON.stringify(msg));
+  }
+
+  private socketHandler() {
     this.wsService.socket
-      .pipe(map(data => JSON.parse(data)), filter(json => json.id === myId), map(data => data.result))
+      .pipe(map(data => JSON.parse(data)))
       .subscribe(data => {
-        // console.log(data);
-        this.dataStore.states = data;
-        this._states.next(Object.assign({}, this.dataStore).states);
-      }, error => console.log('Could not load states'));
-    this.wsService.sendMessage(JSON.stringify({ id: myId, type: 'get_states' }));
-  }
-
-  getId() {
-    return (+ new Date()) * 1000 + this.id++ % 1000;
+        if (this.msgHandler.hasOwnProperty(data.id)) {
+          this.msgHandler[data.id](data);
+        }
+      });
   }
 
 }
